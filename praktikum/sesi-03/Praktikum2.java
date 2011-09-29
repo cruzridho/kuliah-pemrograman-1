1fe2606b10bc221487aee4cb16e37518a2367584
Cruz Ridho Pradana <cruzridho@gmail.com>
2011-09-29 20:57:18 +0700

    praktikum sesi-03

---
+++ praktikum/sesi-03/Praktikum2.java
@@ -0,0 +1,9 @@
+public class Praktikum2{ 
+	public static void main(String[] args){ 
+        int ourfirstnumber = 10; 
+		double ourfirstdouble = 10; 
+         
+		System.out.println(ourfirstnumber);  
+		System.out.println(ourfirstdouble); 
+	} 
+}
---
+++ praktikum/sesi-03/Praktikum6.java
@@ -0,0 +1,10 @@
+	public class Praktikum6{ 
+		public static void main(String[] args){ 
+        
+		int a=10;
+		long b=a;
+
+		long x=5;
+		int y= (int)x;
+	} 
+}
